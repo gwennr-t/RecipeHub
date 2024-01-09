aca23a612be51a072fe719877753b58a574bd129
Gwenn RT <gwenncreative@gwenns-MBP.attlocal.net>
2024-01-08 21:18:56 -0600

    pseudo code in js

--- public/script.js
+++ public/script.js
@@ -5,3 +5,18 @@ import {
   } from "tw-elements";
   
   initTE({ Collapse, Ripple });
+
+  
+  // user input for search
+
+  // fetch function 
+
+  // fetch for food
+
+  // fetch for drinks
+
+  // fetch for both 
+
+  // create cards for items
+
+  // local storage for saved recipes
