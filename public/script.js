1d63a8520675f65f93bbe05a9cb3e7df1d7ff933
Gwenn RT <gwenncreative@gwenns-MBP.attlocal.net>
2024-01-10 18:43:18 -0600

    added in fetch for food

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 
 // user input for search
-var userInput = document.querySelector('.');
+// var userInput = document.querySelector('.');
 var savedRecipes = document.getElementById('savedRecipes');
 var randomMeal = document.getElementById('randomMeal');
 var foodRecipes = document.getElementById('foodRecipes');
@@ -17,20 +17,31 @@ var randomMealFunction = function (){
 
 }
   // fetch function
+
   // fetch for food
 var foodItem = function (food){
-  fetch('URLfor food')
+  fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=e39ea64b56894c6ea15c430bd91edef5&query=' + food + '&addRecipeInformation=true')
   .then(response => response.json())
   .then(data => {
+    var titleValue = data['results']['title'];
+    var imageValue = data['results']['image'];
+    var servingsValue = data['results']['servings'];
+    var readyInMinutesValue = data['results']['readyInMinutes'];
+    var dairyFreeValue = data['results']['dairyFree'];
+    var glutenFreeValue = data['results']['glutenFree'];
+    var descriptionValue = data['results']['summary'];
+
     console.log(data)
   })
   .catch(err => alert("Incorrect food item!"));
 }
   // fetch for drinks
 var drinkItem = function (drink){
-  fetch('URLfod drinks')
+  fetch('www.thecocktaildb.com/api/json/v1/1/search.php?s=' + drink)
   .then(response => response.json())
   .then(data => {
+    
+
     console.log(data)
   })
   .catch(err => alert("Incorrect drink item!"));
@@ -46,6 +57,6 @@ var drinkItem = function (drink){
   window.localStorage.setItem("drinks", JSON.stringify(listOfDrinks));
 
   randomMeal.addEventListener('click',randomMealFunction);
-savedRecipes.addEventListener('click',savedItems);
+  savedRecipes.addEventListener('click',savedItems);
   foodRecipes.addEventListener('click', foodItem);
-  drinkRecipess.addEventListener('click', drinkItem);
+  drinkRecipes.addEventListener('click', drinkItem);
